e681730db0045cc8929f9f38af55e65129ad8b79
George Spanos <gspanos@moby-it.com>
2024-02-03 16:09:04 +0200

    fix: remove local ip

--- server/api/prices.ts
+++ server/api/prices.ts
@@ -11,8 +11,8 @@ export default defineEventHandler(async event => {
   if (!clientIp) return createError('no client ip found on request');
   const prices = await $fetch(regionalPricingUrl, {
     headers: {
-      // 'ip-origin': clientIp,
-      'ip-origin': '2a02:587:9030:d900:f15e:21a6:7583:60fe'
+      'ip-origin': clientIp,
+      // 'ip-origin': '2a02:587:9030:d900:f15e:21a6:7583:'
     },
   });
   const { success, output, issues } = v.safeParse(pricesResponseSchema, prices);
