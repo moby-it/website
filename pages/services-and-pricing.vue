cf3054045c34a1e0feafc9e6bef8da3138f681f9
George Spanos <gspanos@moby-it.com>
2024-02-20 16:17:33 +0200

    fix: remove duplicate text

--- pages/services-and-pricing.vue
+++ pages/services-and-pricing.vue
@@ -43,11 +43,11 @@ function getImgFromIdx(idx: number): string {
   <article>
     <Banner title="No Hidden Costs">
       <template #subtitle>
-        <h4 style="margin-bottom: var(--gap-2);">We provide each of our services at a <NuxtLink to="#services"> fixed montly price</NuxtLink>.</h4>
+        <h4 style="margin-bottom: var(--gap-2);">We provide each of our services at a <NuxtLink to="#services"> fixed
+            montly price</NuxtLink>.</h4>
         <h4>Every contract is tailored to time-bound deliverables and clear deadlines, ensuring no hidden costs. We
           shoulder any unforeseen expenses, guaranteeing you peace of mind and financial predictability from start to
-          finish.</h4>Every contract is tailored to time-bound deliverables and clear deadlines, ensuring no hidden costs. We shoulder
-          any unforeseen expenses, guaranteeing you peace of mind and financial predictability from start to finish.
+          finish.</h4>
       </template>
     </Banner>
     <section class="services" id="services">
