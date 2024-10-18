bd212a2b5b913a72045ec28e5b25c515851df2ae
George Spanos <gspanos@moby-it.com>
2024-10-18 09:41:42 +0300

    add compat date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxt/image', '@nuxtjs/i18n'],
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-        head: {
-            htmlAttrs: {
-                lang: 'en',
-            },
-        },
-    },
-    routeRules: {
-        '/js/script.js': {
-            proxy: {
-                to: 'https://plausible.io/js/script.js',
-            },
-        },
-        '/api/event': {
-            proxy: {
-                to: 'https://plausible.io/api/event',
-            },
-        },
-    },
-    nitro: {
-        routeRules: {
-            '*': {
-                prerender: true,
-            },
-        },
-    },
-    runtimeConfig: {
-        regionalPricingApiKey: '',
-        automailerApiKey: '',
-        public: {
-            automailerUrl: 'https://automailer.moby-it.com/send-email',
-            regionalPricingUrl: 'https://pricing.moby-it.com/regionalPrices',
-        },
-    },
-    devtools: { enabled: true },
+  modules: ['@nuxt/image', '@nuxtjs/i18n'],
+
+  app: {
+      pageTransition: { name: 'page', mode: 'out-in' },
+      head: {
+          htmlAttrs: {
+              lang: 'en',
+          },
+      },
+  },
+
+  routeRules: {
+      '/js/script.js': {
+          proxy: {
+              to: 'https://plausible.io/js/script.js',
+          },
+      },
+      '/api/event': {
+          proxy: {
+              to: 'https://plausible.io/api/event',
+          },
+      },
+  },
+
+  nitro: {
+      routeRules: {
+          '*': {
+              prerender: true,
+          },
+      },
+  },
+
+  runtimeConfig: {
+      regionalPricingApiKey: '',
+      automailerApiKey: '',
+      public: {
+          automailerUrl: 'https://automailer.moby-it.com/send-email',
+          regionalPricingUrl: 'https://pricing.moby-it.com/regionalPrices',
+      },
+  },
+
+  devtools: { enabled: true },
+  compatibilityDate: '2024-10-18',
 });
