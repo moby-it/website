c745c77feb97d2824caaa570104d09daade0a3a7
George Spanos <gspanos@moby-it.com>
2024-10-18 09:53:13 +0300

    deps and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,43 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/image', '@nuxtjs/i18n'],
+    modules: ['@nuxt/image', '@nuxtjs/i18n'],
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' },
+        head: {
+            htmlAttrs: {
+                lang: 'en',
+            },
+        },
+    },
 
-  app: {
-      pageTransition: { name: 'page', mode: 'out-in' },
-      head: {
-          htmlAttrs: {
-              lang: 'en',
-          },
-      },
-  },
+    routeRules: {
+        '/js/script.js': {
+            proxy: {
+                to: 'https://plausible.io/js/script.js',
+            },
+        },
+        '/api/event': {
+            proxy: {
+                to: 'https://plausible.io/api/event',
+            },
+        },
+    },
 
-  routeRules: {
-      '/js/script.js': {
-          proxy: {
-              to: 'https://plausible.io/js/script.js',
-          },
-      },
-      '/api/event': {
-          proxy: {
-              to: 'https://plausible.io/api/event',
-          },
-      },
-  },
+    nitro: {
+        routeRules: {
+            '*': {
+                prerender: true,
+            },
+        },
+    },
 
-  nitro: {
-      routeRules: {
-          '*': {
-              prerender: true,
-          },
-      },
-  },
+    runtimeConfig: {
+        regionalPricingApiKey: '',
+        automailerApiKey: '',
+        public: {
+            automailerUrl: 'https://automailer.moby-it.com/send-email',
+            regionalPricingUrl: 'https://pricing.moby-it.com/regionalPrices',
+        },
+    },
 
-  runtimeConfig: {
-      regionalPricingApiKey: '',
-      automailerApiKey: '',
-      public: {
-          automailerUrl: 'https://automailer.moby-it.com/send-email',
-          regionalPricingUrl: 'https://pricing.moby-it.com/regionalPrices',
-      },
-  },
-
-  devtools: { enabled: true },
-  compatibilityDate: '2024-10-18',
+    devtools: { enabled: true },
 });
